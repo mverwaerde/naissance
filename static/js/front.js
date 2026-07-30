58a2364d0bfcd5799ab702fd4a60fc1357c0820d
Florent Jardin <florent@jardin.im>
2026-07-30 17:36:38 +0200

    refactor: remove unused functions from front.js
    - productDetailGallery() : 0 references in templates
    - productDetailSizes() : 0 references in templates
    - keep alignElementsSameHeight() : still called on load/resize

--- static/js/front.js
+++ static/js/front.js
@@ -61,46 +61,6 @@ function utils() {
         $('body').scrollTo($('#' + trgt), 800, {offset: -80});
     });
 }
-/* product detail gallery */
-function productDetailGallery(confDetailSwitch) {
-    $('.thumb:first').addClass('active');
-    timer = setInterval(autoSwitch, confDetailSwitch);
-    $(".thumb").click(function (e) {
-        switchImage($(this));
-        clearInterval(timer);
-        timer = setInterval(autoSwitch, confDetailSwitch);
-        e.preventDefault();
-    }
-    );
-    $('#mainImage').hover(function () {
-        clearInterval(timer);
-    }, function () {
-        timer = setInterval(autoSwitch, confDetailSwitch);
-    });
-    function autoSwitch() {
-        var nextThumb = $('.thumb.active').closest('div').next('div').find('.thumb');
-        if (nextThumb.length == 0) {
-            nextThumb = $('.thumb:first');
-        }
-        switchImage(nextThumb);
-    }
-    function switchImage(thumb) {
-        $('.thumb').removeClass('active');
-        var bigUrl = thumb.attr('href');
-        thumb.addClass('active');
-        $('#mainImage img').attr('src', bigUrl);
-    }
-}
-/* product detail sizes */
-function productDetailSizes() {
-    $('.sizes a').click(function (e) {
-        e.preventDefault();
-        $('.sizes a').removeClass('active');
-        $('.size-input').prop('checked', false);
-        $(this).addClass('active');
-        $(this).next('input').prop('checked', true);
-    });
-}
 $.fn.alignElementsSameHeight = function () {
     $('.same-height-row').each(function () {
         var maxHeight = 0;
